e15355807e05812b78c6cf34424b9a0a60e6e59c
Kenyo Faria <kenyofaria@gmail.com>
2022-03-30 13:09:24 -0300

    modifications to address some problems when file not exists

--- spring-demo/src/main/java/br/com/meli/springdemo/util/ArquivoUtil.java
+++ spring-demo/src/main/java/br/com/meli/springdemo/util/ArquivoUtil.java
@@ -1,6 +1,7 @@
 package br.com.meli.springdemo.util;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -23,27 +24,35 @@ public class ArquivoUtil<T> {
 	
 	public void escreve(String arquivo) {
 		try {
-			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(arquivo, true));
-			bufferedWriter.append(tipo.toString());
+			File file = getFile(arquivo);
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true));
+			bufferedWriter.write(tipo.toString());
 			bufferedWriter.newLine();
 			bufferedWriter.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
+	private File getFile(String arquivo) throws IOException {
+		File file = new File(arquivo);
+		if(!file.exists())
+			file.createNewFile();
+		return file;
+	}
 	
 	public List<String> leitura(String arquivo){
 		Scanner scanner;
 		List<String> result = new ArrayList<String>();
 		try {
-			scanner = new Scanner(new FileInputStream(arquivo));
+			File file = getFile(arquivo);
+			scanner = new Scanner(new FileInputStream(file));
 			while(scanner.hasNext()) {
 				result.add(scanner.nextLine());
 			}
 			scanner.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
 		return result;
 	}
